0572d948bc06f450498e0412c0a9c29cc38782f0
sinseongseop <sss02030@naver.com>
2024-11-09 23:23:51 +0900

    Fix: ImageEvaluation API 오류 수정

--- src/api/imageEvaluationService.js
+++ src/api/imageEvaluationService.js
@@ -1,19 +1,29 @@
-import { API, FORMAPI } from './axios';
+import { API, FORMAPI } from "./axios";
 
 // 이미지 리스트들 평가 생성 API
 export const createImageEvaluations = (imageFiles) => {
   const formData = new FormData();
   imageFiles.forEach((file, index) => {
-    formData.append(`images[${index}]`, file);
+    formData.append("images", file); // 'images'로 수정
   });
-  return FORMAPI.post('/api/imageEvaluation', formData);
+  return FORMAPI.post("/api/imageEvaluation", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 };
 
 // 특정 이미지 평가 조회 API
 export const getImageEvaluation = (id) => API.get(`/api/imageEvaluation/${id}`);
 
+// 오늘 올린 이미지 평가 조회 API
+export const getTodayImageEvaluations = () =>
+  API.get("/api/imageEvaluation/list/today");
+
 // 최근 5개의 이미지 평가 조회 API
-export const getRecentImageEvaluations = () => API.get('/api/imageEvaluation/list/recent');
+export const getRecentImageEvaluations = () =>
+  API.get("/api/imageEvaluation/list/recent");
 
 // 평가를 요청한 모든 이미지 목록 조회 API
-export const getAllImageEvaluations = () => API.get('/api/imageEvaluation/list/all');
+export const getAllImageEvaluations = () =>
+  API.get("/api/imageEvaluation/list/all");
